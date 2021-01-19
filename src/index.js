7a447cd3d7126467e7da0a5039a199aa04811b63
Ngozi Mbabie <gozij@yahoo.com>
2021-01-19 09:36:16 -0600

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,87 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {
+  BrowserRouter as Router, 
+  Route,
+  Link,
+} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import {Component} from 'react';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Home extends Component {
+  render(){
+    return(
+      <div>
+        <h1>My Travel Blog Site</h1>
+        <h4>Here you will see my favorite Vacation Destination and Sites!</h4>
+        <img src="https://www.freepik.com/free-vector/travel-tourism-illustration-with-resort-sightseeing-elements_6932247.htm#page=1&query=travel&position=14" alt= "Travel"/>
+      </div>
+    );
+  }
+}
+class About extends Component {
+  render(){
+    return(
+      <div>
+        <h1>About</h1>
+        <p>I like travelling to different places. This is because I like exploring new things.</p>
+        <p>Part of the reason I like travelling is because I get to try out different delicacies
+          and meet new people.
+        </p>
+      </div>
+    );
+  }
+}
+
+class MyBlog extends Component {
+  render(){
+    return(
+      <div>
+        <h1>My Blog</h1>
+        <p>I like visiting natural environments, scenes and sites:
+          <ul>
+            <li>Beaches and ocean sites</li>
+            <li>Rocky Mountains</li>
+            <li>Waterfalls</li>
+          </ul>
+        </p>
+      </div>
+    );
+  }
+}
+class Navbar extends Component {
+  render(){
+    return(
+      <div className="nav">
+        <Link to="/">Home</Link> | 
+        <Link to="My Blog">My Blog</Link> | 
+        <Link to="about">About</Link>
+      </div>
+    );
+  }
+}
+
+class Routes extends Component {
+  render(){
+    return(
+      <Router>
+        <div>
+          <Navbar />
+          <hr />
+          <Route name="Home" exact path="/" component={Home}/>
+          <Route name="My Blog" path="/my blog" component={MyBlog}/>
+          <Route name="About" path="/about" component={About} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+ReactDOM.render(<Routes />, document.getElementById('root'));
+
+
+
+
+
+
